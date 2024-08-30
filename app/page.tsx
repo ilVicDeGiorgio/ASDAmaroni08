a102d2e3686945044e2a3c8a8e095538bf0a7146
Rossella Sonzogni <ross.sonzogni@gmail.com>
2024-08-30 17:45:52 +0200

    Minor fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 
 export default function HomePage() {
-  return (
-    <ColorSchemeToggle />
-  );
+  return <ColorSchemeToggle />;
 }
