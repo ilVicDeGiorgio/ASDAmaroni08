6d028f9ab323889690d39d08f5b1f5497846158c
Vittorio De Giorgio <vdegiorgio8@gmail.com>
2024-09-15 19:37:08 +0200

    Updated carousel component

--- components/Carousel/Demo.tsx
+++ components/Carousel/Demo.tsx
@@ -5,11 +5,46 @@ import { Carousel } from '@mantine/carousel';
 import '@mantine/carousel/styles.css'; // Mantine CSS se ancora ne hai bisogno
 import styles from './Demo.module.scss'; // Importa il modulo SCSS
 import Image from 'next/image';
-import logoMilan from '/public/LogoMilan.png';
-import LogoLazio from '/public/LogoLazio.png';
-import LogoInter from '/public/LogoInter.png';
-import LogoRoma from '/public/LogoRoma.png';
-import LogoFanta from '/public/LogoFanta.png';
+
+// Definisci i dati delle partite
+const matches = [
+  {
+    competition: 'Serie A',
+    homeTeam: {
+      name: 'AC Milan',
+      logo: 'https://1000marche.net/wp-content/uploads/2020/03/AC-Milan-Logo-1995.png',
+    },
+    awayTeam: {
+      name: 'SS Lazio',
+      logo: 'https://upload.wikimedia.org/wikipedia/it/archive/6/62/20190806203656%21Stemma_della_Societ%C3%A0_Sportiva_Lazio.svg',
+    },
+    score: '3:1',
+  },
+  {
+    competition: 'Serie A',
+    homeTeam: {
+      name: 'Team45',
+      logo: 'https://www.maestridelcalcio.com/wp-content/uploads/2020/07/real-madrid-logo-png-12.png',
+    },
+    awayTeam: {
+      name: 'Team23',
+      logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Logo_FC_Bayern_M%C3%BCnchen_%282002%E2%80%932017%29.svg/2048px-Logo_FC_Bayern_M%C3%BCnchen_%282002%E2%80%932017%29.svg.png',
+    },
+    score: '2:2',
+  },
+  {
+    competition: 'Serie A',
+    homeTeam: {
+      name: 'Team3',
+      logo: 'https://www.logofantacalcio.it/wp-content/uploads/2024/04/logo-fantacalcio-stemma.png',
+    },
+    awayTeam: {
+      name: 'Team4',
+      logo: 'https://www.logofantacalcio.it/wp-content/uploads/2024/04/logo-fantacalcio-stemma.png',
+    },
+    score: '1:4',
+  },
+];
 
 function Demo(): ReactElement {
   return (
@@ -20,98 +55,43 @@ function Demo(): ReactElement {
       controlsOffset="xs"
       className={styles.carouselWrapper}
     >
-      {/* Slide 1 */}
-      <Carousel.Slide>
-        <div className={styles.carouselSlide}>
-          <div className={styles.header}>
-            Partite recenti
-            <div className={styles.league}>Serie A</div>
-          </div>
-          <div className={styles.content}>
-            <div className={styles.team}>
-              <Image src={logoMilan} alt="Inter Logo" className={styles.logo} />
-              <div className={styles.name}>AC Milan</div>
+      {matches.map((match, index) => (
+        <Carousel.Slide key={index}>
+          <div className={styles.carouselSlide}>
+            <div className={styles.header}>
+              Partite recenti
+              <div className={styles.league}>{match.competition}</div>
             </div>
-            <div className={styles.score}>
-              3<span className={styles.separator}>:</span>1
-            </div>
-            <div className={styles.team}>
-              <Image src={LogoLazio} alt="Milan Logo" className={styles.logo} />
-              <div className={styles.name}>SS Lazio</div>
-            </div>
-          </div>
-        </div>
-      </Carousel.Slide>
-
-      {/* Slide 2 */}
-      <Carousel.Slide>
-        <div className={styles.carouselSlide}>
-          <div className={styles.header}>
-            Partite recenti
-            <div className={styles.league}>Serie A</div>
-          </div>
-          <div className={styles.content}>
-            <div className={styles.team}>
-              <Image
-                src={LogoFanta}
-                alt="Team 1 Logo"
-                className={styles.logo}
-                height={64}
-                width={120}
-              />
-              <div className={styles.name}>Team45</div>
-            </div>
-            <div className={styles.score}>
-              2<span className={styles.separator}>:</span>2
-            </div>
-            <div className={styles.team}>
-              <Image
-                src={LogoFanta}
-                alt="Team 2 Logo"
-                className={styles.logo}
-                height={54}
-                width={100}
-              />
-              <div className={styles.name}>Team23</div>
+            <div className={styles.content}>
+              <div className={styles.team}>
+                <Image
+                  src={match.homeTeam.logo}
+                  alt={match.homeTeam.name}
+                  width={100}
+                  height={64}
+                  className={styles.logo}
+                />
+                <div className={styles.name}>{match.homeTeam.name}</div>
+              </div>
+              <div className={styles.score}>
+                <span className={styles.homeScore}>{match.score.split(':')[0]}</span>
+                <span className={styles.separator}>:</span>
+                <span className={styles.awayScore}>{match.score.split(':')[1]}</span>
+              </div>
+              <div className={styles.team}>
+                <Image
+                  src={match.awayTeam.logo}
+                  alt={match.awayTeam.name}
+                  width={100}
+                  height={64}
+                  className={styles.logo}
+                />
+                <div className={styles.name}>{match.awayTeam.name}</div>
+              </div>
             </div>
           </div>
-        </div>
-      </Carousel.Slide>
-
-      {/* Slide 3 */}
-      <Carousel.Slide>
-        <div className={styles.carouselSlide}>
-          <div className={styles.header}>
-            Partite recenti
-            <div className={styles.league}>Serie A</div>
-          </div>
-          <div className={styles.content}>
-            <div className={styles.team}>
-              <Image
-                src={LogoFanta}
-                alt="Team 3 Logo"
-                className={styles.logo}
-                height={54}
-                width={100}
-              />
-              <div className={styles.name}>Team3</div>
-            </div>
-            <div className={styles.score}>
-              1<span className={styles.separator}>:</span>4
-            </div>
-            <div className={styles.team}>
-              <Image
-                src={LogoFanta}
-                alt="Team 4 Logo"
-                className={styles.logo}
-                height={54}
-                width={80}
-              />
-              <div className={styles.name}>Team4</div>
-            </div>
-          </div>
-        </div>
-      </Carousel.Slide>
+        </Carousel.Slide>
+      ))}
     </Carousel>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,15 @@ export default withBundleAnalyzer({
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
   },
   sassOptions: {
-    prependData: 
-      `@import "./styles/global.scss";`,
+    prependData: `@import "./styles/global.scss";`,
+  },
+
+  images: {
+    domains: [
+      '1000marche.net',
+      'upload.wikimedia.org',
+      'www.maestridelcalcio.com',
+      'www.logofantacalcio.it',
+    ],
   },
 });
