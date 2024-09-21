5373e772f2422bf053fa3ad1db33e8982fb85d6e
Vittorio De Giorgio <vdegiorgio8@gmail.com>
2024-09-21 11:35:27 +0200

    Added font: 'Poppins' and adjusted some CSS rules.

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,12 @@ import Navbar from '../components/Navbar/Navbar';
 import Headbar from '@/components/Headbar/Headbar';
 
 import styles from './layout.module.scss';
+import { Poppins } from '@next/font/google';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+});
 
 export const metadata = {
   title: 'Fc-FantasyClub',
@@ -22,11 +28,13 @@ export default function RootLayout({ children }: { children: any }) {
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
       </head>
-      <body className={clsx(styles.root)}>
-        <Headbar />
-        {/* <MantineProvider theme={theme}>{children}</MantineProvider> */}
-        {children}
-        <Navbar />
+      <body className={(clsx(styles.root), poppins.className)}>
+        <div>
+          <Headbar />
+          {/* <MantineProvider theme={theme}>{children}</MantineProvider> */}
+          {children}
+          <Navbar />
+        </div>
       </body>
     </html>
   );
