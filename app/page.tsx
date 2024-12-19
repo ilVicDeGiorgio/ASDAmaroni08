39991eacad08c5bb49c0321efe370cf34382967c
Vittorio De Giorgio <vdegiorgio8@gmail.com>
2024-12-19 01:54:58 +0100

    Aggiunto main video nella home

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import clsx from 'clsx';
 import Demo from '../components/Carousel/Demo';
 import { MantineProvider } from '@mantine/core';
@@ -11,10 +11,11 @@ import styles from './page.module.scss';
 
 export default function HomePage() {
   const [showAnimation, setShowAnimation] = useState(true);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setShowAnimation(false); // Nasconde l'animazione dopo 5 secondi
+      setShowAnimation(false); // Nasconde l'animazione dopo 2 secondi
     }, 2000);
 
     return () => clearTimeout(timer); // Pulisce il timer quando il componente viene smontato
@@ -32,7 +33,18 @@ export default function HomePage() {
           />
         </div>
       ) : (
-        <div className={styles.postorino2}></div>
+        <main className={styles.postorino2}>
+          <title></title>
+          <video
+            ref={videoRef}
+            src="/movie.mp4" // Sostituisci con il percorso del tuo video
+            className={styles.videoBackground}
+            autoPlay
+            loop
+            muted
+            playsInline
+          />
+        </main>
       )}
     </>
   );
